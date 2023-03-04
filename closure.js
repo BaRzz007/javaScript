61bb52e6ae6e709e0531de7c5a2fa15d09a561d3
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-03-04 14:04:15 +0100

    practicing closure

--- closure.js
+++ closure.js
@@ -4,14 +4,14 @@ function range(start, end) {
 	var list = [];
 	if (end === undefined) {
 		return function(end) {
-			for (let i = start; i <= end; i = i + 1) {
+			for (let i = start; i <= end; i++) {
 				list.push(i);
 			}
 			return list;
 		};
 	}
 	else {
-		for (let i = start; i <= end; i = i + 1) {
+		for (let i = start; i <= end; i++) {
 			list.push(i);
 		}
 	}
@@ -22,3 +22,5 @@ console.log(range(3, 8));
 
 var start3 = range(3);
 console.log(start3(5));
+var start4 = range(4);
+console.log(start4(6));
